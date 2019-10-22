b8ee08d321ac54b5f39343d0dab7c61da5c9f1aa
Steve Degosserie <steve@parity.io>
2019-10-22 10:57:30 +0200

    Use into() iso to_string(); test full graph

--- tests/lib_test.rs
+++ tests/lib_test.rs
@@ -7,7 +7,7 @@ use cargo_deps::{get_dep_graph, render_dep_graph, Config};
 #[test]
 fn get_dep_graph_self() {
     let mut cfg = Config::default();
-    cfg.manifest_path = "../Cargo.toml".to_string();
+    cfg.manifest_path = "Cargo.toml".into();
     let graph = get_dep_graph(cfg).unwrap();
     assert!(graph.nodes.iter().any(|d| d.name == "clap"));
     assert!(graph.nodes.iter().any(|d| d.name == "toml"));
@@ -16,8 +16,11 @@ fn get_dep_graph_self() {
 #[test]
 fn render_dep_graph_self() {
     let mut cfg = Config::default();
-    cfg.manifest_path = "../Cargo.toml".to_string();
+    cfg.manifest_path = "Cargo.toml".into();
     let graph = get_dep_graph(cfg).unwrap();
     let out = render_dep_graph(graph).unwrap();
-    assert!(out.starts_with("digraph dependencies"));
+    assert_eq!(
+        out,
+        "digraph dependencies {\n\tn6 [label=\"cargo-deps\", shape=box];\n\n}\n"
+    );
 }
